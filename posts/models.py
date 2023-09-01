3d036496c137f4305b2c1fc74904f59c2be0ef79
ItsAmmarTariq <ammarch935@gmail.com>
2023-09-01 19:30:36 +0500

    created model post and voter successfully

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title= models.CharField(max_length=50)
+    url=models.URLField(max_length=200) 
+    poster=models.ForeignKey(User, on_delete=models.CASCADE)
+    created=models.DateTimeField(auto_now_add=True)
+
+
+class Meta:
+    ordering=['-created']
